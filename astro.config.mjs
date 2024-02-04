f26bfe62462bfa088f86ca6b7a7b53c7b930fc89
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-02-03 22:45:33 -0600

    bump: astro@2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://indyandie.github.io',
+  site: 'https://indyandie.github.io/',
   base: '/ebishi',
   integrations: [svelte()]
 });
