241ca912323c8eeb398baeaa94d4e2d1bd108a0d
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-25 01:11:11 -0500

    build(pkg): add tailwindcss integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://indyandie.github.io/',
   base: 'ebishi/',
-  integrations: [svelte()]
+  integrations: [svelte(), tailwind()]
 });
