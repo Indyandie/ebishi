edf974fa7d2a6a3f30d17ff93bc70706536f3ef9
A <1058837+Indyandie@users.noreply.github.com>
2024-10-20 23:43:47 -0500

    feat(deno): migrate build and deploy to deno 2   (#3)
    * build(deno): migrate to deno 2 build
    
    * fix(deno): resolve `astro dev` exceeds OS open files limit (os error 24)
    
    issue: https://github.com/denoland/deno/issues/17757
    
    * build(deps): bump astro
    
    * build(deno): action to deploy using deno
    
    * fix(deno): remove broken step (test)
    
    * fix(deno): update step
    
    * fix(deno): add version to action path
    
    * fix: add `uses` to deploy step
    
    * refactor: update extension, deno install scripts, add pages step
    
    * test: add text to validate deployment
    
    * fix: update upload artifact to upload the `dist/` dir not `dist` file
    
    * fix: LHTW
    
    * fix: use correct repo `actions/upload-artifact@v4`
    
    * fix: add id to upload artifact step
    
    * fix: name artifact `github-pages`
    
    * fix: try artifact v3
    
    * fix: revert to artifact v4
    
    * fix: use `actions/upload-pages-artifact@v3`
    
    https://github.com/actions/deploy-pages/issues/345
    
    * chore: remove test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://indyandie.github.io/',
   base: 'ebishi/',
-  integrations: [svelte(), tailwind()]
-});
+  integrations: [svelte(), tailwind()],
+  vite: {
+    server: {
+      watch: {
+        useFsEvents: false,
+        usePolling: true,
+      },
+    },
+  },
+})
