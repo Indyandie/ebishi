801f3213231aaec39a1a6f3641ce7d004d60f982
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-02-01 13:36:38 -0600

    chore: update `tailwindcss` configuratioon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
-
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ebishi.indieandy.cc/',
   integrations: [
     svelte(),
-    tailwind(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
