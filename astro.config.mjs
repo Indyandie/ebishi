920a61e049295bd7a8fb6ba7bf65d0477a3441e2
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-02-02 14:38:44 -0600

    test(deno): add `vite.server` so dev stops freaking out

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,11 @@ export default defineConfig({
   ],
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      watch: {
+        useFsEvents: false,
+        usePolling: true,
+      },
+    },
   },
 })
